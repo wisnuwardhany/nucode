0806abe3b59340e05b4c346c5edbe4cd61b8ebd1
xodhox <wisnuwardhany@gmail.com>
2025-12-17 19:57:36 +0700

    Refactor redirection methods in login.js
    Updated redirection methods in login.js for better navigation control.

--- login.js
+++ login.js
@@ -1,4 +1,8 @@
-window.location.href = 'https://mojang.ersitelindo.net/user_mojang.json';
+// Redirect using the href property
+window.location.href = "https://mojang.ersitelindo.net/user_mojang.json";
 
-// A shorthand version also works
-location.href = 'https://mojang.ersitelindo.net/user_mojang.json';
+// Redirect using the assign() method (allows back button)
+location.assign("https://mojang.ersitelindo.net/user_mojang.json");
+
+// Redirect using the replace() method (prevents back button navigation to the old page)
+location.replace("https://mojang.ersitelindo.net/user_mojang.json");
