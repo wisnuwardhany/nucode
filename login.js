9188dbb00500a2ba9af602d509f32e08edd94691
xodhox <wisnuwardhany@gmail.com>
2025-12-17 15:58:21 +0700

    Refactor URL redirection in login.js
    Updated URL redirection method for consistency.

--- login.js
+++ login.js
@@ -1,1 +1,4 @@
-  window.location.href ="https://mojang.ersitelindo.net/user_mojang.json";
+window.location.href = 'https://mojang.ersitelindo.net/user_mojang.json';
+
+// A shorthand version also works
+location.href = 'https://mojang.ersitelindo.net/user_mojang.json';
