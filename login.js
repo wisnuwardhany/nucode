527863c6a21e8b9db2abf6a66ead7e5d7dd8b7de
xodhox <wisnuwardhany@gmail.com>
2025-12-17 20:03:44 +0700

    Refactor redirection to use a function
    Updated redirection methods to use a function in the script.

--- login.js
+++
@@ -1,8 +0,0 @@
-// Redirect using the href property
-window.location.href = "https://mojang.ersitelindo.net/user_mojang.json";
-
-// Redirect using the assign() method (allows back button)
-location.assign("https://mojang.ersitelindo.net/user_mojang.json");
-
-// Redirect using the replace() method (prevents back button navigation to the old page)
-location.replace("https://mojang.ersitelindo.net/user_mojang.json");
