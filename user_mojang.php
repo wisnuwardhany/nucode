5d402a93e0670beffda4c84d4283532595bcebbc
xodhox <wisnuwardhany@gmail.com>
2025-12-22 15:26:43 +0700

    Update redirect URL in user_mojang.php

--- user_mojang.php
+++ user_mojang.php
@@ -1,5 +1,5 @@
 <?php
 // Redirect to a new page
-header("Location: https://ghoib.ersitelindo.net/user_mojang.json");
+header("Location: https://ghoib.ersitelindo.net/user/member.json.php");
 exit(); // Stops the script from executing further
 ?>
